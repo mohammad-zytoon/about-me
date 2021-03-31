ebee694d8b73d3b09290edb2abe78466d2ff18cb
awwadsaeed <saeedawwad450@gmail.com>
2021-03-31 14:09:17 +0300

    first question

--- js/app.js
+++ js/app.js
@@ -1,92 +1,93 @@
 'use strict'
-let score=0;
-let userName= prompt('Please enter your Name?');
+let score = 0;
+let userName = prompt('Please enter your Name?');
 
 alert(' hi ' + userName + ' you are welcome in our page please answer the next questions');
+function questionOne() {
+    let client = prompt('Did you think am a horses lover?');
 
-let client= prompt('Did you think am a horses lover?');
+    client = client.toUpperCase();
 
-client = client.toUpperCase();
+    if (client == 'YES' || client == 'Y') {
 
-if (client == 'YES' || client == 'Y') {
-    
-    console.log('horse lover');
-    score++;
-    alert('yes i am you will be very happy in my page');
+        console.log('horse lover');
+        score++;
+        alert('yes i am you will be very happy in my page');
 
- }else if (client == 'NO' || client == 'N') {
+    } else if (client == 'NO' || client == 'N') {
 
-    console.log('client status: '+client );
-    alert('wrong i love it you have to take a looke and you will change your mind');
+        console.log('client status: ' + client);
+        alert('wrong i love it you have to take a looke and you will change your mind');
+    }
 }
 
 
-let horseType= prompt('are you think that i thouht arabian horse is the pretiest type?');
+let horseType = prompt('are you think that i thouht arabian horse is the pretiest type?');
 
 horseType = horseType.toUpperCase();
 
 if (horseType == 'YES' || horseType == 'Y') {
-    
+
     console.log('the answer:' + horseType);
     score++;
     alert('we are in the same opp');
 
- }else if (horseType == 'NO' || horseType == 'N') {
+} else if (horseType == 'NO' || horseType == 'N') {
 
     console.log('the answer:' + horseType);
     alert('we are in the different in opp');
 }
 
-let horseRasident= prompt('did you think i have ever raised horses?');
+let horseRasident = prompt('did you think i have ever raised horses?');
 
 horseRasident = horseRasident.toUpperCase();
 
 if (horseRasident == 'YES' || horseRasident == 'Y') {
-    
+
     //console.log('the answer:' + horseRasident);
     score++;
     alert('you are right i am a lucky person');
 
- }else if (horseRasident == 'NO' || horseRasident == 'N') {
+} else if (horseRasident == 'NO' || horseRasident == 'N') {
 
     //console.log('the answer:' + horseRasident);
     alert('no i did you have to buy one');
 }
 
 
-let raider= prompt('do you think i have ever ride a horse?');
+let raider = prompt('do you think i have ever ride a horse?');
 
 raider = raider.toUpperCase();
 
 if (raider == 'YES' || raider == 'Y') {
-    
+
     //console.log('the answer:' + raider);
     score++;
     alert('i am sure its a nice experiment');
 
- }else if (raider == 'NO' || raider == 'N') {
+} else if (raider == 'NO' || raider == 'N') {
 
-   // console.log('the answer:' + raider);
+    // console.log('the answer:' + raider);
     alert('you have to try it');
 }
 
 
-let feedback= prompt('is our web nice?');
+let feedback = prompt('is our web nice?');
 
 feedback = feedback.toUpperCase();
 
 if (feedback == 'YES' || feedback == 'Y') {
-    
+
     //console.log('the answer:' + feedback);
     score++;
     alert('We are glad you shared this answer');
 
- }else if (feedback == 'NO' || feedback == 'N') {
+} else if (feedback == 'NO' || feedback == 'N') {
 
-   // console.log('the answer:' + raider);
+    // console.log('the answer:' + raider);
     alert('If you have any comment, please provide it to us');
 
-    alert(' Than you  ' + userName +'for visiting our page' );
+    alert(' Than you  ' + userName + 'for visiting our page');
 }
 
 
@@ -114,71 +115,69 @@ for (let i = 0; i < 4; i++) {
         
     }*/
 
-    let i=0;
-    do{
-        let numberGess= prompt('please guess the right number between (1-7)?');
-   
-        if (numberGess==4) {
+let i = 0;
+do {
+    let numberGess = prompt('please guess the right number between (1-7)?');
+
+    if (numberGess == 4) {
+        score++;
+        alert('you are good in guessing you choose the rghit number');
+
+        break;
+
+    } else if (numberGess > 4) {
+        alert('Too High');
+
+    } else if (numberGess < 4) {
+        alert('Too Low');
+    }
+
+    if (i == 3) {
+        alert('Sorry you exhausted all attempts the true vlue is: 4');
+    }
+    i++;
+} while (i < 4);
+
+
+
+
+let horsesColor = ['black', 'brown', 'white'];
+
+for (let i = 0; i < 6; i++) {
+
+
+    let clientChoice = prompt('please guess one of the best three colors for horses from this colors\n(red, green, white, black, brown, yellow, grey)?');
+    clientChoice = clientChoice.toLowerCase();
+
+
+    for (let j = 0; j < horsesColor.length; j++) {
+        //console.log(j);
+
+        if (horsesColor[j] === clientChoice) {
             score++;
-            alert('you are good in guessing you choose the rghit number');
-            
+            alert('good answer one of the best');
+            i = 7;
             break;
-       
-        } else if (numberGess>4){
-            alert('Too High');    
-        
-        }else if (numberGess<4){
-            alert('Too Low');
         }
-    
-        if(i==3){
-            alert('Sorry you exhausted all attempts the true vlue is: 4');
-        }
-            i++;
-         }while (i<4);
+    }
 
 
-         
-         
-         let horsesColor= ['black', 'brown', 'white'];
-         
-         for (let i = 0; i < 6; i++) {
-             
 
-            let clientChoice= prompt('please guess one of the best three colors for horses from this colors\n(red, green, white, black, brown, yellow, grey)?');
-            clientChoice = clientChoice.toLowerCase();
-           
-           
-            for (let j = 0; j< horsesColor.length; j++)
-               {
-                     //console.log(j);
+    if (i == 5) {
 
-                     if (horsesColor[j]===clientChoice ) {
-                        score++;
-                        alert('good answer one of the best');
-                         i= 7;
-                         break;
-                     } 
-               }  
-                
-                 
+        alert('Sorry you finish your attempt, the color choices are:\n Black, brown and White');
 
-        if (i==5) {
-            
-            alert ('Sorry you finish your attempt, the color choices are:\n Black, brown and White');
-            
-        }         
-                
-            }
+    }
 
-            alert('well done You have a (' + score + ') correct answer please visit our web always thx');
+}
+
+alert('well done You have a (' + score + ') correct answer please visit our web always thx');
 
 
 
 
-    
 
 
 
 
-    
+
